76135b43bdc6ed61f137bc7b22d71e8cc63f60ea
SureshBritt <sureshornirmal@gmail.com>
2025-01-22 18:46:04 -0600

    initial commit

--- src/test/java/com/suresh/learning/testng/test/FirstTest.java
+++ src/test/java/com/suresh/learning/testng/test/FirstTest.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
 public class FirstTest {
     @Test
     public void MyFirstTest() throws InterruptedException {
@@ -23,5 +25,26 @@ public class FirstTest {
         Assert.assertTrue(LoadedUrl.contains("ebay"),"Assert true if loaded url contains ebay text");
         Assert.assertFalse(LoadedUrl.isEmpty(),"Assert false if loaded url is empty");
         driver.close();
+    }
+    @Test
+    public void MyFirstTestWithSoftAssert() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        System.setProperty("webdriver.chrome.driver", "C:/Automation/drivers/chromedriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.com/");
+        Thread.sleep(3000);
+        driver.findElement(By.id("gh-search-btn")).click();
+        Thread.sleep(3000);
+        String title = driver.getTitle();
+        System.out.println(title);
+        String LoadedUrl = driver.getCurrentUrl();
+        System.out.println(LoadedUrl);
+        softAssert.assertEquals(title,"Shop by Category | eBay","assertions on title equals ");
+        softAssert.assertNotSame(LoadedUrl,"https://www.ebay.com/","assertions on initial url and url after clicking submit are not same");
+        assert LoadedUrl != null;
+        softAssert.assertTrue(LoadedUrl.contains("ebay"),"Assert true if loaded url contains ebay text");
+        softAssert.assertFalse(LoadedUrl.isEmpty(),"Assert false if loaded url is empty");
+        driver.close();
+        softAssert.assertAll();
     }
 }
